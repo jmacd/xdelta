45bc1408336abd3b7bb0e2ee6fdcb44bca759bbf
josh.macdonald <jmacd@users.noreply.github.com>
2006-12-16 06:25:18 +0000

    Fix badcopy.c for WIN32. Adds some real bogus rand logic.

--- xdelta3/badcopy.c
+++ xdelta3/badcopy.c
@@ -15,7 +15,24 @@ usize_t num_change   = 0;
 
 int last_end = 0;
 
-static int
+#ifdef WIN32
+// whatever
+static 
+double drand48() {
+  double r = rand() / (double)RAND_MAX;
+  return r;
+}
+long lrand48() {
+	long l = 0;
+	int i;
+	for (i = 0; i < 32; i++) {
+		l = l ^ (l << 2) ^ (l << 1) ^ rand();
+	}
+	return l;
+}
+#endif
+
+static usize_t
 edist (usize_t mean, usize_t max)
 {
   double mean_d = mean;
@@ -38,7 +55,7 @@ void modify (char *buf, usize_t size)
       /* The expected interval of such a change. */
       double expect_interval = ((double) next_size * (1.0 - error_prob)) / error_prob;
       /* The number of bytes until the next modification. */
-      usize_t next_mod  = edist (expect_interval, 1 << 31);
+      usize_t next_mod  = edist ((usize_t)expect_interval, 1 << 31);
 
       if (next_size + next_mod + bufpos > size) { break; }
 
@@ -47,7 +64,6 @@ void modify (char *buf, usize_t size)
       bufpos += next_mod;
 
       fprintf (stderr, "COPY: %u-%u (%u)\n", total_size + last_end, total_size + bufpos, bufpos - last_end);
-
       fprintf (stderr, "ADD:  %u-%u (%u) is change %u\n", total_size + bufpos , total_size + bufpos + next_size, next_size, num_change);
 
       total_change += next_size;
@@ -55,7 +71,7 @@ void modify (char *buf, usize_t size)
 
       for (j = 0; j < next_size; j += 1, bufpos += 1)
 	{
-	  buf[bufpos] = lrand48 () >> 3;
+	  buf[bufpos] = (char)(lrand48 () >> 3);
 	}
 
       last_end = bufpos;
@@ -68,17 +84,31 @@ void modify (char *buf, usize_t size)
 
 int main(int argc, char **argv)
 {
-  char buf[BUFSZ];
+  FILE *in, *out;
+  char *buf = malloc(BUFSZ);
   int c, ret;
 
-  if (argc > 3)
+  if (argc > 5)
     {
       fprintf (stderr, "usage: badcopy [byte_error_prob [mean_error_size]]\n");
       return 1;
     }
 
-  if (argc > 2) { mean_change = atoi (argv[2]); }
-  if (argc > 1) { error_prob  = atof (argv[1]); }
+  if (argc > 4) { mean_change = atoi (argv[4]); }
+  if (argc > 3) { error_prob  = atof (argv[3]); }
+  fprintf (stderr, "mean change = %u; error_prob = %0.10f\n", mean_change, error_prob);
+
+  in = fopen(argv[1], "rb");
+  out = fopen(argv[2], "wb");
+
+  if (in == NULL) {
+	  fprintf (stderr, "Cannot open input: %s\n", argv[1]);
+	  return 1;
+  }
+  if (out == NULL) {
+	  fprintf (stderr, "Cannot open output: %s\n", argv[2]);
+	  return 1;
+  }
 
   if (error_prob < 0.0 || error_prob > 1.0)
     {
@@ -88,13 +118,13 @@ int main(int argc, char **argv)
 
   do
     {
-      c = fread (buf, 1, BUFSZ, stdin);
+      c = fread (buf, 1, BUFSZ, in);
 
       if (c == 0) { break; }
 
       modify (buf, c);
 
-      ret = fwrite (buf, 1, c, stdout);
+      ret = fwrite (buf, 1, c, out);
     }
   while (c == BUFSZ);
 
