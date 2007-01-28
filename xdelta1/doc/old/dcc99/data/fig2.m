08b0a40be19b61725ac74d5fddbfa7deff0e75b9
josh.macdonald <jmacd@users.noreply.github.com>
2007-01-28 01:27:04 +0000

    Delete junk imported by accident.

--- xdelta1/doc/old/dcc99/data/fig2.m
+++
@@ -1,9 +0,0 @@
-function graphit(name)
-
-plot     (name(:,1), name(:, 2), 'ro')
-hold
-plot     (name(:,1) , name(:, 3), 'bs')
-
-legend ('Vdelta', 'Xdelta')
-ylabel ('Delta Size (bytes)')
-xlabel ('To File Size (bytes)')
