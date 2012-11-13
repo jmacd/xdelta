3489c129e81517a87d2f3c7b66d1e51b2935e5f2
josh.macdonald <jmacd@users.noreply.github.com>
2012-11-13 06:14:31 +0000

    Label 3.0.5

--- xdelta3/xdelta3-main.h
+++ xdelta3/xdelta3-main.h
@@ -356,7 +356,7 @@ static int
 main_version (void)
 {
   /* $Format: "  XPR(NTR \"Xdelta version $Xdelta3Version$, Copyright (C) 2007, 2008, 2009, 2010, 2011, 2012, Joshua MacDonald\\n\");" $ */
-  XPR(NTR "Xdelta version 3.0.4, Copyright (C) 2007, 2008, 2009, 2010, 2011, 2012, Joshua MacDonald\n");
+  XPR(NTR "Xdelta version 3.0.5, Copyright (C) 2007, 2008, 2009, 2010, 2011, 2012, Joshua MacDonald\n");
   XPR(NTR "Xdelta comes with ABSOLUTELY NO WARRANTY.\n");
   XPR(NTR "This is free software, and you are welcome to redistribute it\n");
   XPR(NTR "under certain conditions; see \"COPYING\" for details.\n");
