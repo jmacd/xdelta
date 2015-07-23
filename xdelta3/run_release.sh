e2a8a7b0529c99106783a719cbb0052c2616d6fd
Joshua MacDonald <josh.macdonald@gmail.com>
2015-07-22 23:38:26 -0700

    run_release.sh functions on OS X

--- xdelta3/run_release.sh
+++ xdelta3/run_release.sh
@@ -1,40 +1,41 @@
 #!/bin/bash
 
-# Place C/C++ common flags here
-COMMON="-g"
-
-export CFLAGS
-export CXXFLAGS
-export LDFLAGS
+# Run from the source dir.
+SRCDIR=${PWD}
 
 # TODO replace w/ wget
 LZMA="xz-5.2.1"
-LZMA_FILE="/volume/home/jmacd/src/xdelta-devel/xz-5.2.1.tar.xz"
+LZMA_FILE="${SRCDIR}/../${LZMA}.tar.xz"
 
 MAKEFLAGS="-j 10"
 
-MYOS=`uname`
-
-BUILDDIR=${PWD}/build
+BUILDDIR=${SRCDIR}/build
 LZMASRC=${BUILDDIR}/${LZMA}
 
-LIBBASE=$HOME/lib
+MINGW_CFLAGS="-DEXTERNAL_COMPRESSION=0 -DVCDIFF_TOOLS=0"
+MINGW_CFLAGS="${MINGW_CFLAGS} -DXD3_WIN32=1 -DSHELL_TESTS=0"
 
-MINGW_CFLAGS="-DEXTERNAL_COMPRESSION=0 -DVCDIFF_TOOLS=0 -DXD3_WIN32=1 -DSHELL_TESTS=0"
+MYOS=`uname`
+DATE=`date`
 
-MINGW_LDFLAGS=""
+CLEAN=""
 
-# Location where mingw-w64-build script runs.
-MINGW_BASE="/volume/home/jmacd/src/mingwb"
-
-# Windows 32bit
-# Run from the source dir.
-SRCDIR=$PWD
-
+LINUXTGTS=""
 LINUXTEST1=""
 LINUXTEST2=""
+
+WINTGTS=""
 WINTEST1=""
 WINTEST2=""
+
+OSXTGTS=""
+OSXTEST1=""
+OSXTEST2=""
+
+TMP="${TMP}"
+if [ "${TMP}" == "" ]; then
+    TMP="${TMPDIR}"
+fi
 
 find build -type f 2> /dev/null | xargs rm
 
@@ -51,15 +52,15 @@ function try {
     local dir=$1
     shift
     echo -n "	${w} ..."
-    (cd "${dir}" && "$@" >$w.stdout 2>$w.stderr)
+    (cd "${dir}" && "$@" >${w}.stdout 2>${w}.stderr)
     local s=$?
-    if [ $s -eq 0 ]; then
+    if [ ${s} -eq 0 ]; then
 	echo " success"
     else
 	echo " failed!"
 	echo "Error $1 in ${dir}" >&2
     fi
-    return $s
+    return ${s}
 }
 
 function buildlzma {
@@ -69,7 +70,7 @@ function buildlzma {
 
     echo "	... liblzma"
     
-    mkdir -p $target
+    mkdir -p ${target}
 
     try configure-lzma ${target} ${LZMASRC}/configure \
 	--host=${host} \
@@ -93,18 +94,18 @@ function buildlzma {
 }
 
 function buildit {
-    host=$1
-    march=$2
-    offsetbits=$3
-    cargs=$4
-    largs=$5
-    BM="${host}${march}"
-    D="build/${BM}/xoff${offsetbits}"
-    BMD="${BM}-${offsetbits}"
-    FULLD="$PWD/$D"
-    CFLAGS="${COMMON} ${march} ${cargs} -I${PWD}/build/lib-${BM}/include"
-    CXXFLAGS="${COMMON} ${march} ${cargs} -I${PWD}/build/lib-${BM}/include"
-    LDFLAGS="${largs} ${march} -L${PWD}/build/lib-${BM}/lib"
+    local host=$1
+    local march=$2
+    local offsetbits=$3
+    local cargs=$4
+    local largs=$5
+    local BM="${host}${march}"
+    local D="build/${BM}/xoff${offsetbits}"
+    local BMD="${BM}-${offsetbits}"
+    local FULLD="${SRCDIR}/${D}"
+    local CFLAGS="${march} ${cargs} -g -I${SRCDIR}/build/lib-${BM}/include"
+    local CXXFLAGS="${march} ${cargs} -g -I${SRCDIR}/build/lib-${BM}/include"
+    local LDFLAGS="${largs} ${march} -L${SRCDIR}/build/lib-${BM}/lib"
     mkdir -p ${D}
 
     echo "	... ${BMD}"
@@ -112,6 +113,15 @@ function buildit {
     cat >> Makefile.test <<EOF
 
 # ${BMD}
+# ${CFLAGS}
+.PHONY: build-${BMD}
+build-${BMD}:
+	(cd ${D} && make all)
+
+.PHONY: clean-${BMD}
+clean-${BMD}:
+	(cd ${D} && make clean)
+
 .PHONY: regtest-${BMD}
 regtest-${BMD}:
 	(cd ${D} && ./xdelta3regtest 1> \${TMP}/regtest.${BMD}.stdout 2> \${TMP}/regtest.${BMD}.stderr)
@@ -125,31 +135,42 @@ EOF
 
     case ${host} in
 	*linux*)
+	    LINUXTGTS="${LINUXTGTS} build-${BMD}"
 	    LINUXTEST1="${LINUXTEST1} selftest-${BMD}"
 	    LINUXTEST2="${LINUXTEST2} regtest-${BMD}"
 	    ;;
 	*mingw*)
+	    WINTGTS="${WINTGTS} build-${BMD}"
 	    WINTEST1="${WINTEST1} selftest-${BMD}"
 	    WINTEST2="${WINTEST2} regtest-${BMD}"
 	    ;;
+	*apple*)
+	    OSXTGTS="${OSXTGTS} build-${BMD}"
+	    OSXTEST1="${OSXTEST1} selftest-${BMD}"
+	    OSXTEST2="${OSXTEST2} regtest-${BMD}"
+	    ;;
     esac
+    CLEAN="${CLEAN} clean-${BMD}"
 
-    try configure-xdelta $FULLD $SRCDIR/configure \
+    try configure-xdelta ${FULLD} ${SRCDIR}/configure \
     		  --host=${host} \
     		  --prefix=${FULLD} \
     		  --enable-static \
     		  --disable-shared \
-    		  --enable-debug-symbols
+    		  --enable-debug-symbols \
+		  "CFLAGS=${CFLAGS}" \
+		  "CXXFLAGS=${CXXFLAGS}" \
+		  "LDFLAGS=${LDFLAGS}"
     if [ $? -ne 0 ]; then
 	return
     fi
 
-    try build-xdelta $FULLD make ${MAKEFLAGS} all
+    try build-xdelta ${FULLD} make ${MAKEFLAGS} all
     if [ $? -ne 0 ]; then
 	return
     fi
 
-    try install-xdelta $FULLD make install
+    try install-xdelta ${FULLD} make install
 }
 
 function buildall {
@@ -158,17 +179,8 @@ function buildall {
     echo ""
 
     buildlzma "$1" "$2"
-    if [ $? -ne 0 ]; then
-	return
-    fi
     buildit "$1" "$2" 32 "-DXD3_USE_LARGEFILE64=0 $3" "$4"
-    if [ $? -ne 0 ]; then
-	return
-    fi
     buildit "$1" "$2" 64 "-DXD3_USE_LARGEFILE64=1 $3" "$4"
-    if [ $? -ne 0 ]; then
-	return
-    fi
 }
 
 setup
@@ -178,28 +190,49 @@ if [ $? -ne 0 ]; then
     exit $?
 fi
 
-DATE=`date`
 cat > Makefile.test <<EOF
 # Auto-generated ${DATE} -*- Mode: Makefile -*-
+TMP = ${TMP}
+
 EOF
 
-# Linux
-buildall x86_64-pc-linux-gnu -m32
-buildall x86_64-pc-linux-gnu -m64
+# Native compiles
+if [ "${MYOS}" == "Linux" ]; then
+    # Linux
+    buildall x86_64-pc-linux-gnu -m32
+    buildall x86_64-pc-linux-gnu -m64
+fi
 
-# Windows
-buildall i686-w64-mingw32 -mconsole "${MINGW_CFLAGS}" ""
-buildall x86_64-w64-mingw32 -mconsole "${MINGW_CFLAGS}" ""
+if [ "${MYOS}" == "Darwin" ]; then
+    # OS X
+    buildall x86_64-apple-darwin -m32
+    buildall x86_64-apple-darwin -m64
+fi
+
+# Cross compile
+buildall i686-w64-mingw32 -mconsole "${MINGW_CFLAGS}"
+buildall x86_64-w64-mingw32 -mconsole "${MINGW_CFLAGS}"
 
 cat >> Makefile.test <<EOF
 
-linux: linux-selftest linux-regtest
-windows: windows-selftest windows-regtest
+all: linux windows apple
 
+clean: ${CLEAN}
+
+linux: linux-build linux-selftest linux-regtest
+windows: windows-build windows-selftest windows-regtest
+apple: apple-build apple-selftest apple-regtest
+
+linux-build: ${LINUXTGTS}
 linux-selftest: ${LINUXTEST1}
 linux-regtest: ${LINUXTEST2}
 
+windows-build: ${WINTGTS}
 windows-selftest: ${WINTEST1}
 windows-regtest: ${WINTEST2}
+
+apple-build: ${OSXTGTS}
+apple-selftest: ${OSXTEST1}
+apple-regtest: ${OSXTEST2}
 
 EOF
